efb3da477ca071088f592e1bc6a224c6997d1c26
dhrumi <dondadhrumi@gmail.com>
2025-05-28 21:09:21 +0530

    dhrumi

---
+++ exam_4/Q-2.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+int i,j;
+int count=11;
+ clrscr();
+	  for(i=1; i<=4; i++){
+
+		for(j=1; j <= i; j++){
+		 printf("%d",count++);
+
+		}
+		  printf("\n");
+		}
+
+
+  getch();
+
+}
